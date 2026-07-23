484422a130192380818a2ab02c7dc50c25b64fd8
Jayant Jangid <248914457+Jayant-Jangid@users.noreply.github.com>
2026-07-24 05:22:28 +0530

    Complete 06 exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    fin = ''
+    if (num<0) {
+        return "ERROR"
+    }
+    for (let i=1;i<=num;i++) {
+        fin+=str
+    }
+    return fin
 };
 
 // Do not edit below this line
