1e72a4f9d8021e89f462db65e4bc6c72bcb5e5e1
Jayant Jangid <248914457+Jayant-Jangid@users.noreply.github.com>
2026-07-24 06:11:20 +0530

    Complete 08 exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    let newArr = []
+    for (let stay_val of arr) {
+        if (theArgs.includes(stay_val)) {
+            continue
+        }
+        else {
+            newArr.push(stay_val)
+        }
+    }
+    return newArr
 };
 
 // Do not edit below this line
